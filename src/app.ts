b5dccd4e076565cdd29a1aafe4be4095fb0f689d
Sajjadhosenshohan <mdshohansajjad@gmail.com>
2025-10-01 08:05:28 +0600

    add cors link

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
+
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
-app.use(cors({ origin: ["http://localhost:3000", "https://whatsrealeasy-nu.vercel.app"], credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:3002",
+      "https://whatsrealeasy-nu.vercel.app",
+      "https://www.advanceaimarketing.cloud",
+      "https://advanceaimarketing.cloud",
+      "https://ai.advanceaimarketing.cloud",
+    ],
+    credentials: true,
+  })
+);
 
 // app routes
 app.use("/api/v1", router);
